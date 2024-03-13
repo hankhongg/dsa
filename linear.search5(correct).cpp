d47cb97c7bcc5aeef60069cdf09bce0d97fc4ab7
Không Hân <135267960+hankhongg@users.noreply.github.com>
2024-03-14 00:44:46 +0700

    Create linear.search5(correct).cpp

---
+++ linear.search5(correct).cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void Solution(int& s, int& big)
+{
+    vector<int> a;
+    for (int i=0; i<s; i++){
+        int input;
+        cin >> input;
+        a.push_back(input);
+    }
+
+    vector<int> b;
+    vector<int> c;
+
+    int max1 = *max_element(a.begin(),a.end());
+    int min1 = *min_element(a.begin(),a.end());
+
+    int t=0, t1=0;
+    bool f_max=false, f_min=false;
+
+    for (;t<a.size(); t++){
+        if (a[t]==max1){
+            f_max=true;
+            break;
+        }
+        else b.push_back(a[t]);
+    }
+    if (f_max){
+        t++;
+        for (; t<a.size(); t++){
+            b.push_back(a[t]);
+        }
+    }
+
+    for (;t1<b.size();t1++){
+        if (b[t1]==min1){
+            f_min=true;
+            break;
+        }
+        else c.push_back(b[t1]);
+    }
+    if (f_min){
+        t1++;
+        for (;t1<b.size();t1++){
+            c.push_back(b[t1]);
+        }
+    }
+
+    if (!c.empty()){
+    int max2 = *max_element(c.begin(),c.end());
+    int min2 = *min_element(c.begin(),c.end());
+
+    big = (max1-min1)+(max2-min2);
+    }
+    else big=0;
+
+}
+
+void MaxDifferent(int& n, vector<int>& cases)
+{
+    cin >> n;
+    for (int i=0; i<n; i++){
+        bool done=false;
+        int big=0;
+        int input;
+        cin >> input;
+        if (input==2){
+            vector<int> temp;
+            for (int j=0; j<input; j++){
+                int input2;
+                cin >> input2;
+                temp.push_back(input2);
+            }
+            big = temp[0] > temp[1] ? (temp[0]- temp[1]) : (temp[1] - temp[0]) ;
+            cases.push_back(big);
+            done = true;
+        }
+        if (!done){
+        Solution(input, big);
+        cases.push_back(big);
+        }
+    }
+
+    for (int i=0; i<cases.size(); i++) cout << cases[i] << endl;
+}
+
+int main()
+{
+    vector<int>cases;
+    int n;
+    MaxDifferent(n, cases);
+}
